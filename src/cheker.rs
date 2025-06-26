103b07e2c9aab05b95eab15625fd5ba142396bf3
martin <martin.labat92@gmail.com>
2025-06-26 22:53:35 +0200

    refactor: checker

--- src/cheker.rs
+++ src/cheker.rs
@@ -29,6 +29,7 @@ impl Checker {
 
         let config = Config::new();
         let fan_device = Fan::new(&config);
+
         Self {
             is_init: false,
             config,
@@ -43,88 +44,82 @@ impl Checker {
                 trace!("New fan device detected");
                 self.fan_device = Some(Fan::new_fan_device(path, &self.config));
             } else {
-                error!("Still no new fan device");
+                error!("Still no fan device available");
                 return;
             }
         }
 
         let fan = self.fan_device.as_mut().unwrap();
-
-        let file_content = match fs::read_to_string(format!("{}/{}", fan.path, FILE_NAME_CUR_STATE))
-        {
-            Ok(content) => content,
+        let state_path = format!("{}/{}", fan.path, FILE_NAME_CUR_STATE);
+        let current_speed: u32 = match fs::read_to_string(&state_path) {
+            Ok(content) => match content.trim().parse::<u32>() {
+                Ok(speed) => speed,
+                Err(e) => {
+                    error!("Can't parse speed value: {e}");
+                    return;
+                }
+            },
             Err(e) => {
-                error!("Device is not available {e}");
+                error!("Device is not available: {e}");
                 self.fan_device = None;
                 return;
             }
         };
 
         if self.temp_device.is_none() {
-            if let Ok(new_temp_device) = Temp::new() {
-                trace!("New temp device detected");
-                self.temp_device = Some(new_temp_device);
-            } else {
-                error!("Still no new fan device");
-                return;
+            match Temp::new() {
+                Ok(device) => {
+                    trace!("New temp device detected");
+                    self.temp_device = Some(device);
+                }
+                Err(_) => {
+                    error!("Still no temp device available");
+                    return;
+                }
             }
         }
 
         let temp = self.temp_device.as_ref().unwrap();
-
-        let current_temp: f64 = match temp.get_current_temp() {
+        let current_temp = match temp.get_current_temp() {
             Ok(temp) => temp,
             Err(err) => {
-                error!("Can't read temperature {err}, trying to re-read again");
+                error!("Can't read temperature: {err}");
 
-                match Temp::get_temp_path() {
-                    Ok(path) => {
-                        if let Some(device) = &mut self.temp_device {
-                            device.path = path;
-                        }
-                    }
-                    Err(err) => {
-                        error!("Can't get temp path: {err}");
-                    }
+                if let Ok(path) = Temp::get_temp_path()
+                    && let Some(device) = &mut self.temp_device
+                {
+                    device.path = path;
+                } else {
+                    error!("Can't get temp path");
                 }
 
                 return;
             }
         };
+        debug!("Current temp {current_temp}");
 
-        match file_content.trim().parse::<u32>() {
-            Ok(speed) => {
-                let desired_state: u32 = fan.choose_speed(current_temp, &self.config);
-                if let Some(last_state) = fan.last_state
-                    && last_state == desired_state
-                {
-                    trace!("state didn't change compared to the last time");
-                    return;
-                }
-                if speed != desired_state || !self.is_init {
-                    info!("Adjusting fan speed to {desired_state} (Temp: {current_temp:.2}°C)");
-                    match fs::write(
-                        format!("{}/{FILE_NAME_CUR_STATE}", fan.path),
-                        desired_state.to_string(),
-                    ) {
-                        Ok(_) => fan.last_state = Some(desired_state),
-                        Err(err) => {
-                            error!("Can't set the speed to device {} {err}", fan.path);
-                            self.fan_device = None;
-                        }
-                    }
-                } else {
-                    debug!("Temp: {current_temp:.2}°C, no speed change needed");
-                }
+        let desired_speed = fan.choose_speed(current_temp, &self.config);
+        debug!("Desired speed {desired_speed}");
 
-                if !self.is_init {
-                    debug!("Setting the speed for the first time!");
-                    self.is_init = true;
-                }
+        if fan.last_state == Some(desired_speed) {
+            debug!("State unchanged");
+            return;
+        }
+
+        if current_speed != desired_speed || !self.is_init {
+            if !self.is_init {
+                debug!("Setting the speed for the first time!");
+                self.is_init = true;
             }
-            Err(e) => {
-                error!("Can't parse speed value {e}");
+            info!("Adjusting fan speed to {desired_speed} (Temp: {current_temp:.2}°C)");
+            if fs::write(&state_path, desired_speed.to_string()).is_ok() {
+                fan.last_state = Some(desired_speed);
+            } else {
+                error!("Can't set speed on device {}", fan.path);
+                self.fan_device = None;
             }
+        } else {
+            debug!("Temp: {current_temp:.2}°C, no speed change needed");
         }
     }
 }
